7c1ed07a7699273df8a9a89f2d11e9e2783b2e8e
Miguel Ramos <miguel.ramos@websublime.dev>
2024-07-06 01:47:43 +0100

    feat: git multiple commands

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 #![deny(clippy::all)]
 
-use workspace_node_tools::git::commands::Git;
+use workspace_node_tools::git::conventional::{ConventionalPackage, ConventionalPackageOptions};
 use workspace_node_tools::monorepo::packages::{Monorepo, PackageInfo};
+use workspace_node_tools::git::commands::{Git, Commit, RemoteTags, PublishTagInfo};
 
 #[macro_use]
 extern crate napi_derive;
@@ -29,4 +30,76 @@ pub fn git_fetch_all(cwd: Option<String>) -> bool {
 #[napi(js_name = "executeFetchAllTags")]
 pub fn git_fetch_all_tags(cwd: Option<String>) -> bool {
   Git::fetch_all_tags(cwd).is_ok()
+}
+
+#[napi(js_name = "setCommit")]
+pub fn commit(message: String, body: Option<String>, footer: Option<String>, cwd: Option<String>) -> bool {
+  Git::git_commit(message, body, footer, cwd).is_ok()
+}
+
+#[napi(js_name = "setTag")]
+pub fn tag(tag: String, message: Option<String>, cwd: Option<String>) -> bool {
+  Git::git_tag(tag, message, cwd).is_ok()
+}
+
+#[napi(js_name = "gitPush")]
+pub fn push(cwd: Option<String>) -> bool {
+  Git::git_push(cwd).is_ok()
+}
+
+#[napi(js_name = "getCurrentSha")]
+pub fn current_sha(cwd: Option<String>) -> String {
+  Git::git_current_sha(cwd)
+}
+
+#[napi(js_name = "isWorkdirUnclean")]
+pub fn workdir_unclean(cwd: Option<String>) -> bool {
+  Git::git_workdir_unclean(cwd)
+}
+
+#[napi(js_name = "getDivergedCommit")]
+pub fn diverged_commit(refer: String, cwd: Option<String>) -> Option<String> {
+  Git::get_diverged_commit(refer, cwd)
+}
+
+#[napi(js_name = "getCommitsSince")]
+pub fn commits_since(cwd: Option<String>, since: Option<String>, relative: Option<String>) -> Vec<Commit> {
+    let commits = Git::get_commits_since(cwd, since, relative);
+
+    commits.into()
+}
+
+#[napi(js_name = "getAllFilesChangedSinceSha")]
+pub fn all_files_changed_since_sha(sha: String, cwd: Option<String>) -> Vec<String> {
+    Git::git_all_files_changed_since_sha(sha, cwd)
+}
+
+#[napi(js_name = "getAllFilesChangedSinceTagInfos")]
+pub fn all_files_changed_since_tag_infos(package_info: Vec<PackageInfo>, tag_info: Vec<PublishTagInfo>, cwd: Option<String>) -> Vec<String> {
+    Git::get_all_files_changed_since_tag_infos(package_info, tag_info, cwd)
+}
+
+#[napi(js_name = "getAllFilesChangedSinceBranch")]
+pub fn all_files_changed_since_branch(package_info: Vec<PackageInfo>, branch: String, cwd: Option<String>) -> Vec<String> {
+    Git::get_all_files_changed_since_branch(package_info, branch, cwd)
+}
+
+#[napi(js_name = "getRemoteOrLocalTags")]
+pub fn remote_tags(cwd: Option<String>, local: Option<bool>) -> Vec<RemoteTags> {
+    Git::get_remote_or_local_tags(cwd, local)
+}
+
+#[napi(js_name = "getLastKnownPublishTagInfoForPackage")]
+pub fn last_known_publish_tag_info_for_package(package_info: PackageInfo, cwd: Option<String>) -> Option<PublishTagInfo> {
+    Git::get_last_known_publish_tag_info_for_package(package_info, cwd)
+}
+
+#[napi(js_name = "getLastKnownPublishTagInfoForAllPackages")]
+pub fn last_known_publish_tag_info_for_all_packages(package_info: Vec<PackageInfo>, cwd: Option<String>) -> Vec<Option<PublishTagInfo>> {
+    Git::get_last_known_publish_tag_info_for_all_packages(package_info, cwd)
+}
+
+#[napi(js_name = "getConventionalForPackage")]
+pub fn conventional_for_package(package_info: PackageInfo, no_fetch_all: Option<bool>, cwd: Option<String>, conventional_options: Option<ConventionalPackageOptions>) -> ConventionalPackage {
+    Git::get_conventional_for_package(package_info, no_fetch_all, cwd, conventional_options)
 }
