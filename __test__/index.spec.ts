b1243f02a4c42dbd879a6aa7eaade1e9ebcf405a
Miguel Ramos <miguel.ramos@mercedes-benz.io>
2024-07-19 12:31:53 +0100

    chore: tests

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -2,14 +2,9 @@ import test from 'ava';
 
 import {
   getDefinedPackageManager,
-  PackageManager,
-  getProjectRootPath
+  PackageManager
 } from '../index';
 
 test('get defined package manager', (t) => {
   t.is(getDefinedPackageManager(), PackageManager.Pnpm);
-});
-
-test('get project root path', (t) => {
-  t.is(getProjectRootPath(process.cwd()), process.cwd());
 });
